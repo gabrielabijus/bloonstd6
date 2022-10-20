c955b734d1b7f7be76a2f77d635313e9896eba99
gabrielabijus <95724163+gabrielabijus@users.noreply.github.com>
2022-10-20 17:13:45 -0300

    Add files via upload

--- p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -2,9 +2,28 @@ var dataBase;
 var background_img;
 var gameState = 0;
 var form, game, person;
+var path1, path2, path3, path4, path5, path6, path7, path8, path9, path10, path11, path12;
+var witch;
+var soldier;
+var hunter;
 
 
 function preload(){
+  background_img = loadImage("imagens/grass.gif");
+  path1 = loadImage("imagens/path.gif");
+  path2 = loadImage("imagens/path.gif");
+  path3 = loadImage("imagens/path.gif");
+  path4 = loadImage("imagens/path.gif");
+  path5 = loadImage("imagens/path.gif");
+  path6 = loadImage("imagens/path.gif");
+  path7 = loadImage("imagens/path.gif");
+  path8 = loadImage("imagens/path.gif");
+  path9 = loadImage("imagens/path.gif");
+  path10 = loadImage("imagens/path.gif");
+  path11 = loadImage("imagens/path.gif");
+  path12 = loadImage("imagens/path.gif");
+
+  
 
 }
 
@@ -17,13 +36,21 @@ function setup() {
 }
 
 function draw() {
-  background(0); 
+  background(background_img); 
 
-  
+  if (gameState === 0){
+
+    fill("black");
+    textSize(35);
+    text("Magic Fight", 330, 100);
+  }
 
   if(gameState === 1){
     game.play()
+    
   }
 
+
+  
   drawSprites();
 }
